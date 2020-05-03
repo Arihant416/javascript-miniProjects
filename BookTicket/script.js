48d1b6817b1da5714821efc4365a97a5f1eadd99
Arihant416 <arihantjain416@gmail.com>
2020-05-03 18:06:33 +0530

    Saved Data to Local Storage

--- BookTicket/script.js
+++ BookTicket/script.js
@@ -5,9 +5,26 @@ const total = document.getElementById('total');
 const movieSelect = document.getElementById('movie');
 
 let ticketPrice = +movieSelect.value;
+
+// Save Selected Movie and index price
+function setMovieData(movieIndex, moviePrice) {
+    localStorage.setItem('selectedMovieIndex', movieIndex);
+    localStorage.setItem('selectedMoviePrice', moviePrice);
+}
+
 //Update Total and count
 updateSelectedCount = () => {
     const selectedSeats = document.querySelectorAll('.row .seat.selected');
+
+    //Copy selected seats into an array
+    //Map through an array
+    //return a new array indexes;
+    const seatsIndex = [...selectedSeats].map((seat) =>
+        [...seats].indexOf(seat)
+    );
+
+    localStorage.setItem('selectedSeats', JSON.stringify(seatsIndex));
+
     const selectedSeatsCount = selectedSeats.length;
 
     count.innerText = selectedSeatsCount;
@@ -16,6 +33,7 @@ updateSelectedCount = () => {
 //Movie select event
 movieSelect.addEventListener('change', (e) => {
     ticketPrice = +e.target.value;
+    setMovieData(e.target.selectedIndex, e.target.value);
     updateSelectedCount();
 });
 
